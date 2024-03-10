a7f12cbab9f613c7d114bf1af6ba37c7260d3f56
Morkik <evlosh@gmail.com>
2024-03-10 20:49:27 +0300

    ref

--- lang/create_files.py
+++ lang/create_files.py
@@ -2,35 +2,33 @@ import json
 
 
 def create_py(filename: str, classname: str, data: dict):
+    file_text = ""
 
-    with open(filename, "w", encoding="utf-8") as file:
-        first_row = f"{classname}:\n"
-        file.write(first_row)
+    classname = f"class {classname}:"
+    init_row = "\tdef __init__(self):"
+    super_row = f"\t\tsuper().__init__()"
 
-        for k, v in data.items():
+    for i in (classname, init_row, super_row):
+        file_text += i
+        file_text += "\n"
 
-            if type(v) == str:
-                if v.count("\n") == 0:
-                    row = f"\t{k} = \"{v}\"\n"
-                else:
-                    v = v.replace("\n", "\\n")
-                    row = f"\t{k} = \"{v}\"\n"
+    for k, v in data.items():
+        file_text += f"\t\tself.{k} = {repr(v)}"
+        file_text += "\n"
 
-            elif type(v) == dict:
-                row = f"\t{k} = {v}\n"
-
-            file.write(row)
+    with open(filename, "w") as new:
+        new.write(file_text)
 
 
 def create_all_files(file: str):
     with open(file, "r", encoding="utf-8") as file:
-        data = json.load(file)
+        data: dict = json.load(file)
 
     ru_file = "lang/rus.py"
     en_file = "lang/eng.py"
 
-    ru_class = "class Rus"
-    en_class = "class Eng"
+    ru_class = "Rus"
+    en_class = "Eng"
 
     ru_data = {k: v[0] for k, v in data.items()}
     en_data = {k: v[1] for k, v in data.items()}
