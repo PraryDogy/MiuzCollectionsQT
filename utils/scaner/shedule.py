a43a76a784fcb3d02fd0f0263f31bd9f2ae90b50
evlosh <evlosh@gmail.com>
2024-03-15 08:56:09 +0300

    ref

--- utils/scaner/shedule.py
+++ utils/scaner/shedule.py
@@ -8,21 +8,20 @@ from .thread import ScanerThread, Manager as ScanerThreadManager
 class ScanerShedule(QObject):
     def __init__(self):
         super().__init__()
-        smb_check_ms = 15000
-
         self.smb_wait_timer = QTimer(self)
-        self.smb_wait_timer.setInterval(smb_check_ms)
+        self.smb_wait_timer.setInterval(1500)
+        self.smb_wait_timer.setSingleShot(True)
         self.smb_wait_timer.timeout.connect(self.start_thread)
         
         self.thread_wait_timer = QTimer(self)
-        self.thread_wait_timer.setInterval(smb_check_ms)
+        self.thread_wait_timer.setInterval(1500)
+        self.thread_wait_timer.setSingleShot(True)
         self.thread_wait_timer.timeout.connect(self.wait_thread)
 
-        self.scan_again_timer = QTimer(self)
-        self.scan_again_timer.setInterval(5 * 60 * 1000)
-        # self.scan_again_timer.setInterval(120 * 1000)
-        self.scan_again_timer.setSingleShot(True)
-        self.scan_again_timer.timeout.connect(self.wait_thread)
+        self.next_scan_timer = QTimer(self)
+        self.next_scan_timer.setInterval(5 * 60 * 1000)
+        self.next_scan_timer.setSingleShot(True)
+        self.next_scan_timer.timeout.connect(self.wait_thread)
 
         utils_signals_app.scaner_start.connect(self.wait_thread)
         utils_signals_app.scaner_stop.connect(self.stop_thread)
@@ -31,8 +30,9 @@ class ScanerShedule(QObject):
         self.scaner_thread = False
 
     def wait_thread(self):
+        self.stop_timers()
+
         if not self.scaner_thread or not self.scaner_thread.isRunning():
-            self.thread_wait_timer.stop()
             self.start_thread()
 
         else:
@@ -40,6 +40,8 @@ class ScanerShedule(QObject):
             self.thread_wait_timer.start()
 
     def start_thread(self):
+        self.stop_timers()
+
         if not MainUtils.smb_check():
             print("scaner no smb, 15 sec wait")
             self.smb_wait_timer.start()
@@ -47,12 +49,18 @@ class ScanerShedule(QObject):
         else:
             print("scaner start from shedule")
             self.scaner_thread = ScanerThread()
-            self.smb_wait_timer.stop()
             self.scaner_thread.start()
-            self.scan_again_timer.start()
+
+            self.next_scan_timer.start()
 
     def stop_thread(self):
         ScanerThreadManager.flag = False
+        self.stop_timers()
+
+    def stop_timers(self):
+        self.smb_wait_timer.stop()
+        self.thread_wait_timer.stop()
+        self.next_scan_timer.stop()
 
 
 scaner_app = ScanerShedule()
