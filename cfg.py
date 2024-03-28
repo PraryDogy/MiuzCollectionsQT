b010484e0d61eca4cb3bff629121b425fdcd8642
evlosh <evlosh@gmail.com>
2024-03-28 22:06:58 +0300

    ref

--- cfg.py
+++ cfg.py
@@ -99,7 +99,7 @@ class Dymanic:
         self.date_start_text: str = None # datetime as readable text
         self.date_end_text: str = None # datetime as readable text
         self.lng: Eng = Eng()
-        self.migrate_data = {
+        self.migrate_data: dict = {
             "migrate": False,
             "old_coll": False,
             "new_coll": False
