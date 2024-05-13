f21a932962172d7e14bb4dcbb3c695ddf5950a48
Evgeny Loshkarev <evlosh@gmail.com>
2024-05-13 11:26:55 +0300

    ref

--- widgets/st_bar/main.py
+++ widgets/st_bar/main.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QFrame, QSpacerItem, QWidget
+from PyQt5.QtWidgets import QFrame, QSpacerItem, QWidget, QApplication
 
 from base_widgets import LayoutH, SvgBtn, SvgShadowed
 from cfg import cnf
@@ -40,32 +40,47 @@ class StBar(QFrame):
         self.h_layout.addSpacerItem(QSpacerItem(10, 0))
 
         self.thumb_move = ThumbMove()
-        self.h_layout.addWidget(self.thumb_move)
+        # self.h_layout.addWidget(self.thumb_move)
 
-        # self.h_layout.addSpacerItem(QSpacerItem(10, 0))
-
-        sett_base = QWidget()
-        sett_base.setFixedWidth(60)
-        sett_base.mouseReleaseEvent = self.sett_btn_cmd
-        self.h_layout.addWidget(sett_base)
-
-        sett_layout = LayoutH()
-        sett_base.setLayout(sett_layout)
+        self.h_layout.addSpacerItem(QSpacerItem(10, 0))
 
         sett_widget = SvgBtn("settings.svg", 17)
-        # sett_widget = SvgShadowed("settings.svg", 17, 200)
-        sett_layout.addWidget(sett_widget)
+        sett_widget.mouseReleaseEvent = self.sett_btn_cmd
+        self.h_layout.addWidget(sett_widget)
+
+        self.h_layout.addSpacerItem(QSpacerItem(20, 0))
 
         self.zoom_wid = SvgBtn(self.get_zoom_icon(), 17)
-        # self.zoom_wid = SvgShadowed(self.get_zoom_icon(), 17, 200)
         self.zoom_wid.mouseReleaseEvent = self.zoom_cmd
         self.h_layout.addWidget(self.zoom_wid)
+
+        self.h_layout.addSpacerItem(QSpacerItem(20, 0))
+
+        switch_theme = SvgBtn("switch_theme.svg", 17)
+        switch_theme.mouseReleaseEvent = self.switch_theme_cmd
+        self.h_layout.addWidget(switch_theme)
 
         self.h_layout.addSpacerItem(QSpacerItem(30, 0))
 
     def reload_stbar(self):
         MainUtils.clear_layout(self.h_layout)
         self.init_ui()
+
+    def switch_theme_cmd(self, e):
+        all_widgets = QApplication.allWidgets()
+        widgets = [
+            widget
+            for widget in all_widgets
+            if widget.objectName()
+            ]
+        
+        if "light_theme" in Themes.current:
+            Themes.set_theme("dark_theme")
+        else:
+            Themes.set_theme("light_theme")
+
+        for i in widgets:
+            i.setStyleSheet(Themes.current)
 
     def sett_btn_cmd(self, e):
         Manager.win_settings = WinSettings()
