cb7db9163f4ff311cc76b3aa3659232b311c1c67
evlosh <evlosh@gmail.com>
2024-06-15 21:11:06 +0300

    ref

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import sys
+from PyQt5.QtGui import QShowEvent
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QScrollArea, QPushButton, QGridLayout
 from PyQt5.QtCore import Qt, QRect, QTimer
 
@@ -21,9 +22,11 @@ class ScrollWidget(QWidget):
         layout.addWidget(self.scroll_area)
 
         self.widgets = []
+        self.widgets_coords = {}
 
         for i in range(0, 3):
             my_grid = QWidget()
+            my_grid.setObjectName(f"ноябрь {i}")
             grid_layout.addWidget(my_grid)
             my_grid_layout = QVBoxLayout()
             my_grid.setLayout(my_grid_layout)
@@ -31,7 +34,7 @@ class ScrollWidget(QWidget):
             title = QLabel(text=f"ноябрь {i}")
             my_grid_layout.addWidget(title)
 
-            self.widgets.append(title)
+            self.widgets.append(my_grid)
 
             for i in range(0, 10):
                 test = QLabel(text="test")
@@ -41,20 +44,18 @@ class ScrollWidget(QWidget):
         self.scroll_area.verticalScrollBar().valueChanged.connect(self.on_scroll)
 
     def on_scroll(self, value):
-        for title in self.widgets:
-            if not self.is_widget_visible(title):
-                print(f"{title.text()} is out of view")
-                break
+        try:
+            a = self.widgets_coords[value].objectName()
+            print(a)
+        except KeyError:
+            print(value)
 
-    def is_widget_visible(self, widget):
-        # Получаем видимую область прокручиваемого содержимого
-        visible_rect = self.scroll_area.viewport().rect()
-        # Преобразуем координаты виджета относительно видимой области
-        widget_rect = self.scroll_area.viewport().mapFromGlobal(widget.mapToGlobal(widget.rect().topLeft()))
-        widget_geom = QRect(widget_rect, widget.size())
+    def showEvent(self, a0: QShowEvent | None) -> None:
+        for widget in self.widgets:
+            self.widgets_coords[widget.y()] = widget
+        print(self.widgets_coords)
+        return super().showEvent(a0)
         
-        # Проверяем пересечение областей
-        return visible_rect.intersects(widget_geom)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
