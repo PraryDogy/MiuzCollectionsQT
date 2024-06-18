254d9d9d3e359f958d65252c1519589d93f18cee
Morkik <evlosh@gmail.com>
2024-06-18 17:49:00 +0300

    fref

--- widgets/thumbnails/thumbnail.py
+++ widgets/thumbnails/thumbnail.py
@@ -1,8 +1,8 @@
 import os
 
-from PyQt5.QtCore import QEvent, QMimeData, QObject, Qt, QUrl
+from PyQt5.QtCore import QEvent, QMimeData, Qt, QUrl
 from PyQt5.QtGui import QContextMenuEvent, QDrag
-from PyQt5.QtWidgets import QAction, QApplication, QFrame, QLabel
+from PyQt5.QtWidgets import QAction, QApplication, QFrame, QLabel, QSpacerItem
 
 from base_widgets import ContextMenuBase, LayoutV
 from cfg import cnf
@@ -45,85 +45,6 @@ class SelectableLabel(QLabel):
         MainUtils.copy_text(self.selectedText().replace("\n", ""))
 
 
-# class Thumbnail(QLabel):
-#     def __init__(self, byte_array: bytearray, img_src: str, coll: str, images_date: str):
-#         super().__init__()
-
-#         self.img_src = img_src
-#         self.coll = coll
-#         self.images_date = images_date
-#         self.img_name = os.path.basename(img_src)
-
-#         cnf.images.append(img_src)
-
-#         self.setObjectName(Names.thumbnail_normal)
-#         self.setStyleSheet(Themes.current)
-
-#         byte_array = PixmapThumb(byte_array)
-
-#         self.setPixmap(byte_array)
-#         self.image_context = None
-
-#     def mouseReleaseEvent(self, event):
-#         Manager.win_image_view = WinImageView(parent=self, img_src=self.img_src)
-#         Manager.win_image_view.show()
-
-#     def mousePressEvent(self, event):
-#         if event.button() == Qt.LeftButton:
-#             self.drag_start_position = event.pos()
-#         super().mousePressEvent(event)
-
-#     def mouseMoveEvent(self, event):
-#         if event.buttons() != Qt.LeftButton:
-#             return
-
-#         distance = (event.pos() - self.drag_start_position).manhattanLength()
-
-#         if distance < QApplication.startDragDistance():
-#             return
-
-#         self.drag = QDrag(self)
-#         self.mime_data = QMimeData()
-#         self.drag.setPixmap(self.pixmap())
-        
-#         url = [QUrl.fromLocalFile(self.img_src)]
-#         self.mime_data.setUrls(url)
-
-#         self.drag.setMimeData(self.mime_data)
-#         self.drag.exec_(Qt.CopyAction)
-
-#     def contextMenuEvent(self, ev: QContextMenuEvent | None) -> None:
-#         try:
-#             self.image_context = ImageContext(img_src=self.img_src, event=ev, parent=self)
-#             self.image_context.closed.connect(self.closed_context)
-#             self.image_context.add_preview_item()
-#             self.setObjectName(Names.thumbnail_selected)
-#             self.setStyleSheet(Themes.current)
-#             self.image_context.show_menu()
-#             return super().contextMenuEvent(ev)
-#         except Exception as e:
-#             print(e)
-
-#     def closed_context(self):
-#         try:
-#             self.setObjectName(Names.thumbnail_normal)
-#             self.setStyleSheet(Themes.current)
-#         except Exception as e:
-#             print(e)
-
-#     def enterEvent(self, a0: QEvent | None) -> None:
-#         self.setToolTip(
-#             f"{self.images_date}\n"
-#             f"{cnf.lng.collection}: {self.coll}"
-#             f"\n{cnf.lng.file_name}: {self.img_name}"
-#             )
-#         return super().enterEvent(a0)
-    
-#     def leaveEvent(self, a0: QEvent | None) -> None:
-#         self.setToolTip("")
-#         return super().leaveEvent(a0)
-
-
 class Thumbnail(QFrame):
     def __init__(self, byte_array: bytearray, img_src: str, coll: str, images_date: str):
         super().__init__()
@@ -142,12 +63,12 @@ class Thumbnail(QFrame):
         self.setObjectName(Names.thumbnail_normal)
         self.setStyleSheet(Themes.current)
 
+        self.v_layout.addSpacerItem(QSpacerItem(0, 7))
+
         self.img_label = QLabel()
         byte_array = PixmapThumb(byte_array)
         self.img_label.setPixmap(byte_array)
         self.v_layout.addWidget(self.img_label, alignment=Qt.AlignmentFlag.AlignCenter)
-
-        self.setFixedWidth(byte_array.width() + 4)
 
         max_chars = 30
         name = '\n'.join(
@@ -155,10 +76,14 @@ class Thumbnail(QFrame):
                  for i in range(0, len(self.img_name), max_chars)]
                  )
         self.title = SelectableLabel(parent=self, text=name)
-        self.title.setContentsMargins(3, 2, 3, 7)
+        self.title.setContentsMargins(8, 2, 8, 7)
 
         self.title.setAlignment(Qt.AlignmentFlag.AlignTop)
         self.v_layout.addWidget(self.title)
+
+        self.setFixedWidth(cnf.THUMBSIZE + 16)
+        self.setMaximumHeight(cnf.THUMBSIZE + 50)
+        
 
     def mouseReleaseEvent(self, event):
         Manager.win_image_view = WinImageView(parent=self, img_src=self.img_src)
