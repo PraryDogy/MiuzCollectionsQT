48b8293802cbf5fdc96ec5a87b38f176a8b93282
Evgeny Loshkarev <evlosh@gmail.com>
2024-09-02 15:08:25 +0300

    ref

--- _icon_convert.py
+++ _icon_convert.py
@@ -5,9 +5,12 @@ import icnsutil
 
 def convert():
     if os.path.exists("icon/icon.png"):
+        print("convert to png")
         img = icnsutil.IcnsFile()
-        img.add_media(file="icon.png")
+        img.add_media(file="icon/icon.png")
         img.write("icon/icon.icns")
         os.remove("icon/icon.png")
 
-        
+
+
+# convert()
