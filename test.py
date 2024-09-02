159ac07732a35753333f3742dce82524c4a59392
Evgeny Loshkarev <evlosh@gmail.com>
2024-09-02 15:55:33 +0300

    123

--- test.py
+++ test.py
@@ -1,45 +1,41 @@
-from difflib import SequenceMatcher
-import os
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QListView, QVBoxLayout, QWidget, QScroller, QScrollerProperties
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
 
-class FindTiffBase:
-    def __init__(self, src: str):
+class BounceScrollApp(QWidget):
+    def __init__(self):
         super().__init__()
-        self.src = src
 
-    def similar(self, jpg: str, tiff: str):
-        return SequenceMatcher(None, jpg, tiff).ratio()
+        layout = QVBoxLayout(self)
 
-    def get_result(self):
-        root, jpg = os.path.split(self.src)
-        jpg_no_ext, ext = jpg.split(".")
+        # Создаем QListView как пример
+        self.list_view = QListView()
+        layout.addWidget(self.list_view)
 
-        files = {
-            f: f.split(".")[0]
-            for f in os.listdir(root)
-            if f.lower().endswith((".tiff", ".tif", ".psd", ".psb"))
-            }
+        # Модель данных для примера
+        model = QStandardItemModel()
+        for i in range(50):
+            item = QStandardItem(f'Item {i + 1}')
+            model.appendRow(item)
+        self.list_view.setModel(model)
 
-        files_ratio = {
-            tiff_ext: self.similar(jpg_no_ext, tiff_no_ext)
-            for tiff_ext, tiff_no_ext in files.items()
-            }
-        
-        files_ratio = {
-            k: v
-            for k, v in files_ratio.items()
-            if v > 0.7
-        }
-        
-        try:
-            return max(files_ratio, key=files_ratio.get)
-        except Exception:
-            return ""
+        # Включаем отскакивание при скроллинге
+        scroller = QScroller.scroller(self.list_view.viewport())
+        scroller.grabGesture(self.list_view.viewport(), QScroller.LeftMouseButtonGesture)
 
+        # Настройка поведения скроллинга через QScrollerProperties
+        properties = scroller.scrollerProperties()
+        properties.setScrollMetric(QScrollerProperties.OvershootDragResistanceFactor, 0.5)
+        properties.setScrollMetric(QScrollerProperties.OvershootScrollDistanceFactor, 0.3)
+        properties.setScrollMetric(QScrollerProperties.OvershootScrollTime, 0.5)
+        scroller.setScrollerProperties(properties)
 
+        self.setLayout(layout)
+        self.setWindowTitle('Bounce Scroll Example')
+        self.setGeometry(300, 300, 300, 400)
 
-
-img = "/Users/Morkowik/Desktop/Evgeny/sample images/tiff_psd_images 2/2021-10-14 11-33-17 (B,Radius4,Smoothing2).jpg"
-a = FindTiffBase(src=img)
-b = a.get_result()
-
-print(b)
+if __name__ == '__main__':
+    app = QApplication([])
+    window = BounceScrollApp()
+    window.show()
+    app.exec_()
