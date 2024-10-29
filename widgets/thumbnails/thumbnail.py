2fe41d607ccd3a8e5e86a12066e9bab3141638b3
Evgeny Loshkarev <evlosh@gmail.com>
2024-10-29 17:37:17 +0300

    ref

--- widgets/thumbnails/thumbnail.py
+++ widgets/thumbnails/thumbnail.py
@@ -38,22 +38,22 @@ class NameLabel(QLabel):
         self.setText(f"{coll}\n{name}\n{ext}")
 
 
-class Thumbnail(QFrame):
+class BaseThumb(QFrame):
     select = pyqtSignal(str)
     open_in_view = pyqtSignal(str)
 
     def __init__(self, byte_array: bytearray, src: str, coll: str, images_date: str):
         super().__init__()
+
         self.row, self.col = 0, 0
-
-        self.v_layout = LayoutV()
-        self.v_layout.setContentsMargins(0, 2, 0, 0)
-        self.setLayout(self.v_layout)
-
         self.src = src
         self.coll = coll
         self.images_date = images_date
         self.img_name = os.path.basename(src)
+
+        self.v_layout = LayoutV()
+        self.v_layout.setContentsMargins(0, 2, 0, 0)
+        self.setLayout(self.v_layout)
 
         cnf.images[src] = {
             "widget": self,
@@ -71,21 +71,15 @@ class Thumbnail(QFrame):
         self.img_label.setPixmap(byte_array)
         self.v_layout.addWidget(self.img_label, alignment=Qt.AlignmentFlag.AlignCenter)
 
-        self.title = NameLabel(parent=self, filename=self.img_name, coll=coll)
-        self.title.setContentsMargins(8, 5, 8, 7)
-
-        self.title.setAlignment(Qt.AlignmentFlag.AlignTop)
-        self.v_layout.addWidget(self.title)
-
-        self.setFixedWidth(cnf.THUMBSIZE + cnf.THUMBPAD)
-        self.setMaximumHeight(cnf.THUMBSIZE + 75)
-
         self.setToolTip(
             f"{cnf.lng.collection}: {self.coll}\n"
             f"{cnf.lng.file_name}: {self.img_name}\n"
             f"{self.images_date}"
-            )
-    
+            )  
+
+        self.setFixedWidth(cnf.THUMBSIZE + cnf.THUMBPAD)
+        self.setMaximumHeight(cnf.THUMBSIZE + 75)
+
     def mouseDoubleClickEvent(self, a0: QMouseEvent | None) -> None:
         self.select.emit(self.src)
         self.open_in_view.emit(self.src)
@@ -135,6 +129,20 @@ class Thumbnail(QFrame):
         except Exception as e:
             MainUtils.print_err(parent=self, error=e)
 
+
+class Thumbnail(BaseThumb):
+    select = pyqtSignal(str)
+    open_in_view = pyqtSignal(str)
+
+    def __init__(self, byte_array: bytearray, src: str, coll: str, images_date: str):
+        super().__init__(byte_array, src, coll, images_date)
+   
+        self.title = NameLabel(parent=self, filename=self.img_name, coll=coll)
+        self.title.setContentsMargins(8, 5, 8, 7)
+
+        self.title.setAlignment(Qt.AlignmentFlag.AlignTop)
+        self.v_layout.addWidget(self.title)
+    
     def selected_style(self):
         for i in (self, self.title):
             i.setObjectName(Names.thumbnail_selected)
@@ -146,88 +154,14 @@ class Thumbnail(QFrame):
             i.setStyleSheet(Themes.current)
 
 
-class SmallThumbnail(QLabel):
+class SmallThumbnail(BaseThumb):
     select = pyqtSignal(str)
     open_in_view = pyqtSignal(str)
 
     def __init__(self, byte_array: bytearray, src: str, coll: str, images_date: str):
-        super().__init__()
-        self.row, self.col = 0, 0
-        self.setContentsMargins(8, 9, 8, 8)
-
-        self.src = src
-        self.coll = coll
-        self.images_date = images_date
-        self.img_name = os.path.basename(src)
-
-        cnf.images[src] = {
-            "widget": self,
-            "collection": coll,
-            "filename": self.img_name
-            }
-
-        self.setObjectName(Names.thumbnail_normal)
-        self.setStyleSheet(Themes.current)
-
-        byte_array = PixmapFromBytes(byte_array)
-        self.setPixmap(byte_array)
-
-        self.setFixedWidth(cnf.THUMBSIZE + cnf.THUMBPAD)
-        self.setMaximumHeight(cnf.THUMBSIZE + 75)
-
-        self.setToolTip(
-            f"{cnf.lng.collection}: {self.coll}\n"
-            f"{cnf.lng.file_name}: {self.img_name}\n"
-            f"{self.images_date}"
-            )
-
-    def mouseReleaseEvent(self, ev: QMouseEvent | None) -> None:
-        self.select.emit(self.src)
-        return super().mouseReleaseEvent(ev)
-
-    def mouseDoubleClickEvent(self, a0: QMouseEvent | None) -> None:
-        self.open_in_view.emit(self.src)
-        return super().mouseDoubleClickEvent(a0)
-
-    def mousePressEvent(self, a0: QMouseEvent | None) -> None:
-        if a0.button() == Qt.MouseButton.LeftButton:
-            self.drag_start_position = a0.pos()
-        return super().mousePressEvent(a0)
-
-    def mouseMoveEvent(self, a0: QMouseEvent | None) -> None:
-        if a0.button() == Qt.MouseButton.RightButton:
-            return
-
-        distance = (a0.pos() - self.drag_start_position).manhattanLength()
-
-        if distance < QApplication.startDragDistance():
-            return
-
-        self.select.emit(self.src)
-        self.drag = QDrag(self)
-        self.mime_data = QMimeData()
-        self.drag.setPixmap(self.pixmap())
-        
-        url = [QUrl.fromLocalFile(self.src)]
-        self.mime_data.setUrls(url)
-
-        self.drag.setMimeData(self.mime_data)
-        self.drag.exec_(Qt.DropAction.CopyAction)
-
-        return super().mouseMoveEvent(a0)
-
-    def contextMenuEvent(self, ev: QContextMenuEvent | None) -> None:
-        try:
-            self.image_context = ImageContext(img_src=self.src, event=ev, parent=self)
-            self.image_context.add_preview_item()
-            if cnf.curr_coll == cnf.ALL_COLLS:
-                self.image_context.add_show_coll_item(collection=self.coll)
-
-            self.select.emit(self.src)
-            self.image_context.show_menu()
-            return super().contextMenuEvent(ev)
-        except Exception as e:
-            MainUtils.print_err(parent=self, error=e)
+        super().__init__(byte_array, src, coll, images_date)
+        self.setContentsMargins(0, 0, 0, 8)
+        self.v_layout.setContentsMargins(0, 0, 0, 0)
 
     def selected_style(self):
         self.setObjectName(Names.thumbnail_selected)
