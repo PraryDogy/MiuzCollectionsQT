5ee595b6353544c87971f0c0e6ff7aab95b2b337
Evgeny Loshkarev <evlosh@gmail.com>
2024-10-30 15:18:59 +0300

    ref

--- test.py
+++ test.py
@@ -1,5 +1,22 @@
 from utils import ImageUtils, ResizeImg
+from database import Dbase, ThumbsMd
+import sqlalchemy
 
-src = "/Users/Loshkarev/Desktop/BF-9х16-0001.jpg"
+
+src = "/Users/Loshkarev/Desktop/S29 E53 R66 N028/2024-10-14 21-43-49.tif"
 img = ImageUtils.read_image(src)
 img = ResizeImg.crop_to_square(img)
+img = ResizeImg.resize_aspect_ratio(img, 200)
+
+
+import cv2
+cv2.imshow("123", img)
+cv2.waitKey(0)
+
+
+# sess = Dbase.get_session()
+# byte_img = ImageUtils.image_array_to_bytes(img)
+# q = sqlalchemy.insert(ThumbsMd).values({"img150": byte_img})
+# sess.execute(q)
+# sess.commit()
+# sess.close()
--- utils/image_utils.py
+++ utils/image_utils.py
@@ -74,7 +74,6 @@ class ImageUtils:
             img = tifffile.imread(files=path)[:,:,:3]
             if str(object=img.dtype) != "uint8":
                 img = (img/256).astype(dtype="uint8")
-            # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             return img
         except (tifffile.TiffFileError, RuntimeError) as e:
             cls.print_error(cls, e)
@@ -100,7 +99,6 @@ class ImageUtils:
     def read_jpg(cls, path: str) -> np.ndarray | None:
         try:
             image = cv2.imread(path, cv2.IMREAD_UNCHANGED)
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             return image
         except (Exception, cv2.error) as e:
             cls.print_error(cls, e)
@@ -118,7 +116,6 @@ class ImageUtils:
                 converted = (rgb_channels * alpha_channel[:, :, np.newaxis] + background * (1 - alpha_channel[:, :, np.newaxis])).astype(np.uint8)
             else:
                 converted = image
-            converted = cv2.cvtColor(converted, cv2.COLOR_BGR2RGB)
             return converted
         
         except Exception as e:
@@ -145,6 +142,9 @@ class ImageUtils:
         else:
             img = None
 
+        if src_lower.endswith((".psd", ".psb", ".tiff", ".tif")) and isinstance(img, np.ndarray):
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
         return img
     
     @classmethod
@@ -169,7 +169,7 @@ class ImageUtils:
     @classmethod
     def image_array_to_bytes(cls, image: np.ndarray, quality: int = 80) -> bytes | None:
         if isinstance(image, np.ndarray):
-            img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            img = image
             res, buffer = cv2.imencode(".jpeg", img, [int(cv2.IMWRITE_JPEG_QUALITY), quality])
             image_io = io.BytesIO()
             image_io.write(buffer)
@@ -217,102 +217,3 @@ class ImageUtils:
         print("ERROR:", error_message)
         print("#" * 100)
         print()
-
-
-# class ReadImage:
-#     def __init__(self, src: str, desaturate_value: float) -> io.BytesIO:
-#         super().__init__()
-
-#         img = cv2.imread(src, cv2.IMREAD_UNCHANGED)
-
-#         if img.shape[-1] == 4:
-#             img = self.read_transparent_png(img)
-
-#         if len(img.shape) == 2:
-#             self.rgb_image = img
-#             return
-
-#         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-#         hsv[:, :, 1] = hsv[:, :, 1] * desaturate_value
-#         self.rgb_image = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-
-#     def read_transparent_png(self, image) -> np.ndarray:
-#         alpha_channel = image[:, :, 3]
-#         rgb_channels = image[:, :, :3]
-
-#         # Преобразуйте значения альфа-канала в диапазон от 0 до 1
-#         alpha_factor = alpha_channel[:, :, np.newaxis] / 255.0
-
-#         # Установите цвет фона (в данном случае, черный)
-#         background_color = np.array([0, 0, 0], dtype=np.uint8)
-
-#         # Создайте изображение фона
-#         background = np.full_like(rgb_channels, background_color)
-
-#         # Сложите изображение и фон
-#         return (
-#             rgb_channels * alpha_factor + background * (1 - alpha_factor)
-#             ).astype(np.uint8)
-
-#     def get_bgr_image(self):
-#         return self.rgb_image
-    
-#     def get_rgb_image(self) -> bytearray:
-#         return cv2.cvtColor(self.rgb_image, cv2.COLOR_BGR2RGB)
-
-
-# class UndefBytesThumb(io.BytesIO,  MainUtils):
-#     def __init__(self):
-#         super().__init__()
-#         img = cv2.imread("images/thumb.jpg")
-#         res, buffer = cv2.imencode(".JPEG", img)
-#         self.write(buffer)
-
-
-# class BaseBytesThumb(io.BytesIO):
-#     def __init__(self, src: str) -> io.BytesIO:
-#         super().__init__()
-
-#         img = ReadImage(src=src, desaturate_value=0.85)
-#         img = img.get_bgr_image()
-
-#         resized = self.fit_thumb(img)
-#         res, buffer = cv2.imencode(".JPEG", resized)
-#         self.write(buffer)
-
-#     def fit_thumb(self, img):
-#         height, width = img.shape[:2]
-#         min_side = min(height, width)
-#         scale_factor = cnf.THUMBSIZE / min_side
-#         new_height = int(height * scale_factor)
-#         new_width = int(width * scale_factor)
-#         return cv2.resize(
-#             img,
-#             (new_width, new_height),
-#             interpolation=cv2.INTER_AREA
-#             )
-
-
-# class BytesThumb(io.BytesIO):
-#     def __init__(self, img_src: str) -> None:
-#         super().__init__()
-
-#         try:
-#             bytes_thumb = BaseBytesThumb(img_src)
-#             self.write(bytes_thumb.getvalue())
-
-#         except Exception as e:
-#             MainUtils.print_err(parent=self, error=e)
-        
-#         if not self.getvalue():
-#             try:
-#                 heif = HeifImage(img_src)
-#                 bytes_thumb = BaseBytesThumb(heif.converted_img_path)
-#                 self.write(bytes_thumb.getvalue())
-#                 os.remove(heif.converted_img_path)
-
-#             except Exception as e:
-#                 MainUtils.print_err(parent=self, error=e)
-            
-#         if not self.getvalue():
-#             raise ZeroDivisionError("Unable to open or convert image.")
