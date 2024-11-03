f553d1250465dcd514230dcc3339df18a980d3b8
Morkik <evlosh@gmail.com>
2024-11-03 17:12:56 +0300

    ref

--- start.py
+++ start.py
@@ -3,32 +3,48 @@ import subprocess
 import sys
 import traceback
 
+class System_:
 
-def catch_err(exc_type, exc_value, exc_traceback):
+    @classmethod
+    def catch_error(cls, *args) -> None:
 
-    ERROR_MSG = "".join(traceback.format_exception(exc_type, exc_value, exc_traceback))
-    APP_NAME: str = "MiuzCollections"
+        STARS = "*" * 40
+        ABOUT = "Отправьте это сообщение в telegram @evlosh или на почту loshkarev@miuz.ru"
+        ERROR = traceback.format_exception(*args)
 
-    FILE_: str = os.path.join(
-        os.path.expanduser("~"),
-        "Library",
-        "Application Support",
-        APP_NAME + "QT",
-        "error.txt"
-        )
+        SUMMARY_MSG = "\n".join([*ERROR, STARS, ABOUT])
+        APP_NAME: str = "MiuzCollections"
 
-    with open(FILE_, "w")as f:
-        f.write(ERROR_MSG)
+        FILE_: str = os.path.join(
+            os.path.expanduser("~"),
+            "Library",
+            "Application Support",
+            APP_NAME + "QT",
+            "error.txt"
+            )
 
-    subprocess.run(["open", FILE_])
+        with open(FILE_, "w")as f:
+            f.write(SUMMARY_MSG)
+
+        subprocess.run(["open", FILE_])
+
+    @classmethod
+    def set_plugin_path(cls) -> bool:
+        #lib folder appears when we pack this project to .app with py2app
+        if os.path.exists("lib"): 
+            plugin_path = "lib/python3.11/PyQt5/Qt5/plugins"
+            os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = plugin_path
+            return True
+        else:
+            return False
+        
+    @classmethod
+    def set_excepthook(cls) -> None:
+        sys.excepthook = cls.catch_error
 
 
-if os.path.exists("lib"): 
-    #lib folder appears when we pack this project to .app with py2app
-    plugin_path = "lib/python3.11/PyQt5/Qt5/plugins"
-    os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = plugin_path
-    sys.excepthook = catch_err
-    print(f"plugin path enabled")
+if System_.set_plugin_path():
+    System_.set_excepthook()
 
 
 from PyQt5.QtCore import QEvent, QObject
