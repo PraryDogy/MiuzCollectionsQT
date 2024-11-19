39ebb4b4c148afe6f029dac025d4126645375986
Morkik <evlosh@gmail.com>
2024-11-19 23:32:55 +0300

    ref

---
+++ lang.py
@@ -0,0 +1,306 @@
+class Lang:
+    add_fav = [
+        "Добавить в избранное",
+        "Add to favorites"
+    ]
+    adding = [
+        "Добавляю",
+        "Add"
+    ]
+    all_colls = [
+        "Все коллекции",
+        "All collections"
+    ]
+    apply = [
+        "Применить",
+        "Apply"
+    ]
+    bar_menu = [
+        "Меню",
+        "Menu"
+    ]
+    browse = [
+        "Обзор",
+        "Browse"
+    ]
+    cancel = [
+        "Отмена",
+        "Cancel"
+    ]
+    changed = [
+        "Изменен",
+        "Changed"
+    ]
+    choose_coll_smb = [
+        "Нет подключения:\n- Подключитесь к /Marketing/Design или\n   к /Studio/Photo. \n- Добавьте новый путь к коллекциям\n   в настройках.",
+        "No connection:\n- Connect to /Marketing/Design or\n   to /Studio/Photo.\n- Add a new path to collections\n   in the settings."
+    ]
+    collection = [
+        "Коллекция",
+        "Collection"
+    ]
+    copy = [
+        "Копировать",
+        "Copy"
+    ]
+    copy_all = [
+        "Копировать все",
+        "Copy all"
+    ]
+    copy_path = [
+        "Скопировать путь",
+        "Copy filepath"
+    ]
+    copying = [
+        "Копирую",
+        "Copying"
+    ]
+    cut = [
+        "Вырезать",
+        "Cut"
+    ]
+    d_m_y = [
+        "день.месяц.год",
+        "day.month.year"
+    ]
+    dates = [
+        "Даты",
+        "Dates"
+    ]
+    del_fav = [
+        "Удалить из избранного",
+        "Remove from favorites"
+    ]
+    deleting = [
+        "Удаляю",
+        "Deleting"
+    ]
+    download_update = [
+        "Скачать обновление",
+        "Update app"
+    ]
+    end = [
+        "Конец",
+        "End"
+    ]
+    fav_coll = [
+        "Избранное",
+        "Favorites"
+    ]
+    file_name = [
+        "Имя файла",
+        "Filename"
+    ]
+    file_size = [
+        "Размер",
+        "Size"
+    ]
+    filter_name = [
+        "Имя фильтра",
+        "Filter name"
+    ]
+    filter_value = [
+        "Значение фильтра",
+        "Filter value"
+    ]
+    from_ = [
+        "из",
+        "from"
+    ]
+    info = [
+        "Инфо",
+        "Info"
+    ]
+    lang_label = [
+        "Язык/Language",
+        "Language/Язык"
+    ]
+    lang_name = [
+        "rus",
+        "eng"
+    ]
+    loading = [
+        "Загрузка...",
+        "Loading..."
+    ]
+    months = [
+        [
+            "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
+        ],
+        [
+            "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+        ]
+    ]
+    months_genitive_case = [
+        [
+            "января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"
+        ],
+        [
+            "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"
+        ]
+    ]
+    no_connection = [
+        "Нет подключения",
+        "No connection"
+    ]
+    no_photo = [
+        "Нет фотографий",
+        "No photos"
+    ]
+    no_photo_filter = [
+        "Нет фотографий в категории",
+        "No photos in category"
+    ]
+    ok = [
+        "Ок",
+        "Ok"
+    ]
+    open_settings_window = [
+        "Открыть настройки",
+        "Open settings window"
+    ]
+    paste = [
+        "Вставить",
+        "Paste"
+    ]
+    photo = [
+        "Фото",
+        "Photo"
+    ]
+    photos_named_dates = [
+        "Фото за даты",
+        "Photo from"
+    ]
+    place = [
+        "Место",
+        "Place"
+    ]
+    reload_gui = [
+        "Обновить",
+        "Update"
+    ]
+    reset = [
+        "Сброс",
+        "Reset"
+    ]
+    reset_dates = [
+        "Сбросить даты",
+        "Dates reset"
+    ]
+    reset_search = [
+        "Очистить поиск",
+        "Reset search"
+    ]
+    resol = [
+        "Разрешение",
+        "Resolution"
+    ]
+    restore_db = [
+        "Восстановить БД",
+        "Restore DB"
+    ]
+    reveal = [
+        "Показать",
+        "Reveal"
+    ]
+    reveal_in_finder = [
+        "Показать в Finder",
+        "Reveal in Finder"
+    ]
+    save_all_JPG = [
+        "Сохранить все JPG",
+        "Save all JPG"
+    ]
+    save_all_JPG_as = [
+        "Сохранить все JPG как ...",
+        "Save all JPG as ..."
+    ]
+    save_all_layers = [
+        "Сохранить все слои",
+        "Save all layers"
+    ]
+    save_all_layers_as = [
+        "Сохранить все слои как ...",
+        "Save all layers as ..."
+    ]
+    save_image_downloads = [
+        "Сохранить в загрузки",
+        "Save to Downloads"
+    ]
+    save_image_in = [
+        "Сохранить как...",
+        "Save as..."
+    ]
+    search = [
+        "Поиск",
+        "Search"
+    ]
+    search_dates = [
+        "Поиск фотографий по датам.\nУправляйте датами с клавишами вверх/вниз.",
+        "Searching for photos by dates.\nControl dates with up/down keys."
+    ]
+    sett_stopcolls = [
+        "Игнорировать коллекции",
+        "Ignore collections"
+    ]
+    settings = [
+        "Настройки",
+        "Settings"
+    ]
+    show_about = [
+        "О приложении",
+        "Show about"
+    ]
+    show_all = [
+        "Показать все",
+        "Show all"
+    ]
+    show_app_support = [
+        "Показать файлы",
+        "Show files"
+    ]
+    show_more = [
+        "Показать еще",
+        "Show more"
+    ]
+    start = [
+        "Начало",
+        "Start"
+    ]
+    title_downloads = [
+        "Загрузки",
+        "Downloads"
+    ]
+    total = [
+        "Всего",
+        "Total"
+    ]
+    type_ = [
+        "Тип",
+        "Type"
+    ]
+    view = [
+        "Просмотр",
+        "View"
+    ]
+    wait_update = [
+        "Подождите...",
+        "Wait..."
+    ]
+    weekdays = [
+        [
+            "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"
+        ],
+        [
+            "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"
+        ]
+    ]
+    where_to_look_coll_folder = [
+        "Где искать папку с коллекциями",
+        "Where to look for a collections folder"
+    ]
+    with_name = [
+        "с именем",
+        "with name"
+    ]
+...
