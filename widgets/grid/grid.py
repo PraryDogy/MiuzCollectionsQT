bfce34574adbe88531ed31859fecd8d3018ece32
Morkik <evlosh@gmail.com>
2024-11-26 21:48:35 +0300

    ref

--- widgets/grid/grid.py
+++ widgets/grid/grid.py
@@ -56,9 +56,7 @@ class Grid(QScrollArea):
         self.resize_timer.timeout.connect(self.rearrange)
 
         self.curr_cell: tuple = (0, 0)
-        self.curr_short_src: str = None
         self.cell_to_wid: dict[tuple, Thumbnail] = {}
-        # self.current_widgets: dict[QGridLayout, list[Thumbnail]] = {}
 
         scroll_wid = QWidget(parent=self)
         self.setWidget(scroll_wid)
@@ -72,7 +70,7 @@ class Grid(QScrollArea):
 
         self.signals_cmd(flag="reload")
 
-        SignalsApp.all_.thumbnail_select.connect(self.select_new_widget)
+        SignalsApp.all_.thumbnail_select.connect(self.select_wid)
         SignalsApp.all_.grid_thumbnails_cmd.connect(self.signals_cmd)
         SignalsApp.all_.win_img_view_open_in.connect(self.open_in_view)
 
@@ -82,11 +80,13 @@ class Grid(QScrollArea):
         elif flag == "to_top":
             self.verticalScrollBar().setValue(0)
         elif flag == "reload":
-            self.load_db_data(flag="first")
+            self.load_db_images(flag="first")
         else:
             raise Exception("widgets > grid > main > wrong flag", flag)
+        
+        self.setFocus()
 
-    def load_db_data(self, flag: str):
+    def load_db_images(self, flag: str):
         """flag: first, more"""
 
         if flag == "first":
@@ -109,10 +109,9 @@ class Grid(QScrollArea):
         if hasattr(self, "grid_wid"):
             self.grid_wid.deleteLater()
 
-        self.curr_cell: tuple = (0, 0)
-        self.curr_short_src = None
-        self.cell_to_wid.clear()
-        Thumbnail.path_to_wid.clear()
+        self.deselect_wid()
+        self.reset_curr_cell()
+        self.clear_grid_data()
         self.up_btn.hide()
 
         self.grid_wid = QWidget()
@@ -150,18 +149,18 @@ class Grid(QScrollArea):
         for date, db_images in db_images.items():
             self.single_grid(date, db_images)
 
-        # scroll_wid.setFocus()
-
     def single_grid(self, date: str, db_images: list[DbImage]):
         max_col = self.get_max_col()
 
         self.col = 0
         title = Title(title=date, db_images=db_images)
-        title.r_click.connect(self.reset_selection)
-        title.row, title.col = self.row, self.col
-        self.grid_lay.addWidget(title, self.row, self.col, 1, max_col)
+        title.r_click.connect(self.deselect_wid)
 
-        self.cell_to_wid[self.row, self.col] = title
+        coords = self.row, self.col
+        self.set_curr_sell(coords, title)
+        self.set_wid_coords(coords, title)
+
+        self.grid_lay.addWidget(title, self.row, self.col, 1, max_col)
         self.row += 1
 
         for db_image in db_images:
@@ -172,9 +171,12 @@ class Grid(QScrollArea):
                 coll=db_image.coll,
                 fav=db_image.fav
             )
-            wid.select.connect(lambda w=wid: self.select_new_widget(w))
-            wid.row, wid.col = self.row, self.col
-            self.cell_to_wid[self.row, self.col] = wid
+            wid.select.connect(lambda w=wid: self.select_wid(w))
+
+            coords = self.row, self.col
+            self.set_curr_sell(coords, wid)
+            self.set_wid_coords(coords, wid)
+
             Thumbnail.path_to_wid[wid.short_src] = wid
 
             self.grid_lay.addWidget(wid, self.row, self.col)
@@ -194,15 +196,31 @@ class Grid(QScrollArea):
             for date, db_images in db_images.items():
                 self.single_grid(date, db_images)
 
-    def select_prev_widget(self):
+    def get_curr_cell(self):
+        return self.cell_to_wid.get(self.curr_cell)
+
+    def reset_curr_cell(self):
+        self.curr_cell = (0, 0)
+
+    def set_curr_sell(self, coords: tuple, wid: Thumbnail | Title):
+        self.cell_to_wid[coords] = wid
+
+    def set_wid_coords(self, coords: tuple, wid: Thumbnail | Title):
+        wid.row, wid.col = coords
+
+    def select_old_wid(self):
         """
         после изменения сетки попытаться найти виджет из предыдущей сетки
         """
-        wid = Thumbnail.path_to_wid.get(self.curr_short_src)
+        wid = self.get_curr_cell()
         if wid:
-            self.select_new_widget(wid)
+            self.select_wid(wid)
 
-    def select_new_widget(self, data: tuple | str | Thumbnail):
+    def clear_grid_data(self):
+        self.cell_to_wid.clear()
+        Thumbnail.path_to_wid.clear()
+
+    def select_wid(self, data: tuple | str | Thumbnail):
         if isinstance(data, Thumbnail):
             coords = data.row, data.col
             new_wid = data
@@ -222,28 +240,27 @@ class Grid(QScrollArea):
                 offset = 1
             
             data = (data[0] + offset, data[1])
-            self.select_new_widget(data)
+            self.select_wid(data)
 
         elif isinstance(new_wid, Thumbnail):
-            self.reset_selection()
+            self.deselect_wid()
             new_wid.selected_style()
             self.ensureWidgetVisible(new_wid)
 
             self.curr_cell = coords
-            self.curr_short_src = new_wid.short_src
 
             if isinstance(BarBottom.path_label, QLabel):
                 t = f"{new_wid.collection}: {new_wid.name}"
                 BarBottom.path_label.setText(t)
 
-    def reset_selection(self):
-        widget = self.cell_to_wid.get(self.curr_cell)
+    def deselect_wid(self):
+        wid = self.get_curr_cell()
 
-        if isinstance(widget, Thumbnail | Title):
-            widget.regular_style()
+        if isinstance(wid, Thumbnail | Title):
+            wid.regular_style()
 
-            if isinstance(BarBottom.path_label, QLabel):
-                BarBottom.path_label.setText("")
+        if isinstance(BarBottom.path_label, QLabel):
+            BarBottom.path_label.setText("")
 
     def open_in_view(self, wid: Thumbnail):
         wid = Thumbnail.path_to_wid.get(wid.short_src)
@@ -275,8 +292,7 @@ class Grid(QScrollArea):
             return
 
         self.ww = self.width()
-        self.cell_to_wid.clear()
-        Thumbnail.path_to_wid.clear()
+        self.clear_grid_data()
 
         max_col = self.get_max_col()
         self.row, self.col = 0, 0
@@ -286,14 +302,20 @@ class Grid(QScrollArea):
             if isinstance(wid, Title):
                 self.col = 0
                 self.row += 1
-                wid.row, wid.col = self.row, self.col
-                self.cell_to_wid[self.row, self.col] = wid
+
+                coords = self.row, self.col
+                self.set_curr_sell(coords, wid)
+                self.set_wid_coords(coords, wid)
+
                 self.grid_lay.addWidget(wid, self.row, self.col, 1, max_col)
                 self.row += 1
 
             elif isinstance(wid, Thumbnail):
-                wid.row, wid.col = self.row, self.col
-                self.cell_to_wid[self.row, self.col] = wid
+
+                coords = self.row, self.col
+                self.set_curr_sell(coords, wid)
+                self.set_wid_coords(coords, wid)
+
                 Thumbnail.path_to_wid[wid.short_src] = wid
                 self.grid_lay.addWidget(wid, self.row, self.col)
                 self.col += 1
@@ -302,11 +324,11 @@ class Grid(QScrollArea):
                 self.col = 0
                 self.row += 1        
         
-        self.select_prev_widget()
+        self.select_old_wid()
 
     def keyPressEvent(self, a0: QKeyEvent | None) -> None:
         if a0.modifiers() & Qt.KeyboardModifier.ControlModifier and a0.key() == Qt.Key.Key_I:
-            wid = Thumbnail.path_to_wid.get(self.curr_short_src)
+            wid = self.get_curr_cell()
             coll_folder = Utils.get_coll_folder(JsonData.brand_ind)
 
             if coll_folder:
@@ -319,30 +341,30 @@ class Grid(QScrollArea):
                 OpenWins.smb(self.window())
 
         elif a0.key() in (Qt.Key.Key_Space, Qt.Key.Key_Return):
-            wid = Thumbnail.path_to_wid.get(self.curr_short_src)
+            wid = self.get_curr_cell()
             if wid:
                 self.open_in_view(wid)
 
         elif a0.key() == Qt.Key.Key_Left:
             coords = (self.curr_cell[0], self.curr_cell[1] - 1)
-            self.select_new_widget(coords)
+            self.select_wid(coords)
 
         elif a0.key() == Qt.Key.Key_Right:
             coords = (self.curr_cell[0], self.curr_cell[1] + 1)
-            self.select_new_widget(coords)
+            self.select_wid(coords)
 
         elif a0.key() == Qt.Key.Key_Up:
             coords = (self.curr_cell[0] - 1, self.curr_cell[1])
-            self.select_new_widget(coords)
+            self.select_wid(coords)
 
         elif a0.key() == Qt.Key.Key_Down:
             coords = (self.curr_cell[0] + 1, self.curr_cell[1])
-            self.select_new_widget(coords)
+            self.select_wid(coords)
         
         return super().keyPressEvent(a0)
 
     def mouseReleaseEvent(self, a0: QMouseEvent | None) -> None:
-        self.reset_selection()
+        self.deselect_wid()
 
     def resizeEvent(self, a0: QResizeEvent | None) -> None:
         self.resize_timer.stop()
@@ -351,7 +373,7 @@ class Grid(QScrollArea):
         return super().resizeEvent(a0)
 
     def contextMenuEvent(self, a0: QContextMenuEvent | None) -> None:
-        self.reset_selection()
+        self.deselect_wid()
         self.menu_ = ContextCustom(event=a0)
 
         reload = ScanerRestart(self.menu_)
@@ -371,4 +393,4 @@ class Grid(QScrollArea):
             self.up_btn.hide()
 
         if value == self.verticalScrollBar().maximum():
-            self.load_db_data(flag="more")
+            self.load_db_images(flag="more")
