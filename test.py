c7e0a0f99e23ae25a008f6c1f05a841540d200f0
Morkik <evlosh@gmail.com>
2024-11-30 18:37:49 +0300

    ref

--- test.py
+++ test.py
@@ -22,8 +22,7 @@ def timer_(func: callable):
         start = time()
         res = func(*args, **kwargs)
         end = time() - start
-        print(end)
-        return res
+        return end, res
 
     return wrapper
 
@@ -42,9 +41,7 @@ class Test:
         finder_images: list[tuple[str, int, int, int]] = []
 
         for root, _, files in os.walk(collection):
-
             for file in files:
-
                 if file.endswith(IMG_EXT):
                     src = os.path.join(root, file)
                     item = self.get_image_item(src)
@@ -54,22 +51,43 @@ class Test:
         return finder_images
     
     @timer_
-    def pathlib_collection(self, collection: str) -> list[tuple[str, int, int, int]]:
-        finder_images: list[tuple[str, int, int, int]] = []
+    # def find_files_with_extensions(self, directory):
+    #     result = []
+    #     stack = [directory]
+    #     while stack:
+    #         current_dir = stack.pop()
+    #         with os.scandir(current_dir) as entries:
+    #             for entry in entries:
+    #                 if entry.is_dir():
+    #                     stack.append(entry.path)
+    #                 elif entry.is_file() and any(entry.name.endswith(ext) for ext in IMG_EXT):
+    #                     result.append(entry.path)
+    #     return result
 
-        for path in Path(collection).rglob("*"):
-
-            if path.suffix.lower() in IMG_EXT:
-
-                finder_images.append((str(path), path.stat()))
-
-        
-        return finder_images
-
+    @timer_
+    def tree2list(self, directory: str) -> list:
+        tree = []
+        for i in os.scandir(directory):
+            if i.is_dir():
+                tree.append(i.path)
+                tree.extend(self.tree2list(i.path))
+            else:
+                tree.append(i.path)
+        return tree
 
 
 src = "/Users/Morkowik/Desktop/Evgeny/_miuz"
-a = Test()
+test = Test()
+walk = test.walk_collection(src)
+scan_dir = test.tree2list(src)
 
-walk = a.walk_collection(src)
-path_lib = a.pathlib_collection(src)
+times = {
+    "walk": walk[0],
+    "scandir": scan_dir[0]
+}
+
+faster = min(times, key=times.get)
+slower = max(times, key=times.get)
+offset = round(times.get(slower) / times.get(faster), 2)
+offset = f"{faster} is faster than {slower} in {offset} times"
+print(offset, sep="\n")
