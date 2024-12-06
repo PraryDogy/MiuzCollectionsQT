ec06a617f00ad00904c2eb5a6ae3d9df92e78371
Morkik <evlosh@gmail.com>
2024-12-07 00:29:39 +0300

    ref

--- widgets/grid/above_thumbs.py
+++ widgets/grid/above_thumbs.py
@@ -158,9 +158,12 @@ class FilterTitle(QWidget):
         self.v_layout.addWidget(h_wid)
 
         if any((Dynamic.date_start, Dynamic.date_end)):
-            title.setText(
-                f"{Lang.photos_named_dates}: {Dynamic.f_date_start} - {Dynamic.f_date_end}"
-                )
+            t = [
+                f"{Lang.photos_named_dates}: ",
+                f"{Dynamic.f_date_start} - {Dynamic.f_date_end}"
+            ]
+            t = "".join(t)
+            title.setText(t)
             h_layout.addWidget(ResetDatesBtn())
 
             spacer = QSpacerItem(1, 10)
