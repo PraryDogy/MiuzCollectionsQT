201d7417a4fde0c3a31828c9c985c68bacbfef78
Morkik <evlosh@gmail.com>
2024-12-08 20:02:48 +0300

    ref

--- base_widgets/input.py
+++ base_widgets/input.py
@@ -9,49 +9,49 @@ from .context import ContextCustom
 
 class CustomContext(ContextCustom):
     def __init__(self, parent: QLineEdit, event):
-        super().__init__(event=event)
-        self.my_parent = parent
-        self.setFixedWidth(120)
+        ...
 
-        sel = QAction(text=Lang.cut, parent=self)
-        sel.triggered.connect(self.cut_selection)
-        self.addAction(sel)
 
-        sel_all = QAction(text=Lang.copy, parent=self)
-        sel_all.triggered.connect(self.copy_selection)
-        self.addAction(sel_all)
-
-        sel_all = QAction(text=Lang.paste, parent=self)
-        sel_all.triggered.connect(self.paste_text)
-        self.addAction(sel_all)
-
-    def copy_selection(self):
-        text = self.my_parent.selectedText()
-        Utils.copy_text(text)
-
-    def cut_selection(self):
-        text = self.my_parent.selectedText()
-        Utils.copy_text(text)
-        self.my_parent.clear()
-
-    def paste_text(self):
-        text = Utils.paste_text()
-        self.my_parent.insert(text)
 
 
 class ULineEdit(QLineEdit):
     def __init__(self):
         """
-        custom copy paste context menu
-
-        height 28
-
-        padding left right 2px
+        custom copy paste context menu, height 28
+        padding left 2, padding right 2px
+        close btn onTextChanged
         """
         super().__init__()
         self.setFixedHeight(28)
         self.setStyleSheet("padding-left: 2px; padding-right: 2px;")
 
+    def cut_selection(self, *args):
+        text = self.selectedText()
+        Utils.copy_text(text)
+
+        new_text = self.text().replace(text, "")
+        self.setText(new_text)
+
+    def paste_text(self, *args):
+        text = Utils.paste_text()
+        self.insert(text)
+
     def contextMenuEvent(self, a0: QContextMenuEvent | None) -> None:
-        self.context_menu = CustomContext(parent=self, event=a0)
-        self.context_menu.show_menu()
+        self.menu_ = ContextCustom(event=a0)
+        self.setFixedWidth(120)
+
+        sel = QAction(text=Lang.cut, parent=self.menu_)
+        sel.triggered.connect(self.cut_selection)
+        self.menu_.addAction(sel)
+
+        sel_all = QAction(text=Lang.copy, parent=self.menu_)
+        sel_all.triggered.connect(
+            lambda: Utils.copy_text(self.selectedText())
+        )
+        self.menu_.addAction(sel_all)
+
+        sel_all = QAction(text=Lang.paste, parent=self.menu_)
+        sel_all.triggered.connect(self.paste_text)
+        self.menu_.addAction(sel_all)
+
+        self.menu_.show_menu()
