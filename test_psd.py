05e4756c3bd98369f9bb6523488a9cec002eae1e
Morkik <evlosh@gmail.com>
2024-12-08 22:15:48 +0300

    ref

--- test_psd.py
+++ test_psd.py
@@ -10,24 +10,28 @@ psd_logger = logging.getLogger("psd_tools")
 psd_logger.setLevel(logging.CRITICAL)
 
 
-def read_psd(path: str):
+def read_psd(path: str) -> np.ndarray | None:
+
     with open(path, 'rb') as psd_file:
 
+        # Проверяем, что файл имеет правильную подпись PSD/PSB
         if psd_file.read(4) != b'8BPS':
             return None
 
+        # Читаем количество каналов из заголовка
+        # Смещаем указатель на байты, содержащие число каналов
         psd_file.seek(12)
         channels = int.from_bytes(psd_file.read(2), byteorder='big')
+
+        # Возвращаем указатель в начало файла
         psd_file.seek(0)
 
         try:
             if channels > 3:
                 img = psd_tools.PSDImage.open(psd_file)
                 img = img.composite()
-
             else:
                 img = Image.open(psd_file)
-        
         except Exception as e:
             print("utils > error read psd", path)
             return None
