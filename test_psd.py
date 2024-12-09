7906dfccae1de6b4cc2f507ab34c04e85c977028
Evgeny Loshkarev <evlosh@gmail.com>
2024-12-09 12:47:29 +0300

    ref

--- test_psd.py
+++ test_psd.py
@@ -5,10 +5,4 @@ import cv2
 src = "/Users/Loshkarev/Desktop/ыа.psb"
 
 
-# img = ReadImage.read_psd(path=src)
-
-
-from PIL import Image
-
-
-img = Image.open(src)
+img = ReadImage.read_psb(src)
--- utils/utils.py
+++ utils/utils.py
@@ -152,8 +152,6 @@ class ReadImage(Err):
             # Это важно, так как мы изменяли положение указателя для проверки структуры файла.
             psd_file.seek(0)
 
-            print(channels)
-
             try:
 
                 if channels > 3:
@@ -165,11 +163,24 @@ class ReadImage(Err):
 
             except Exception as e:
 
-                # print("utils > error read psd", "src:", path, "channels: ", channels)
-                # print(e)
+                print("utils > error read psd", "src:", path)
+                print(e)
                 return None
 
             return np.array(img)
+        
+    @classmethod
+    def read_psb(cls, path: str):
+
+        try:
+            img = psd_tools.PSDImage.open(path)
+            img = img.composite()
+            return np.array(img)
+
+        except Exception as e:
+            print("utils > error read psd", "src:", path)
+            print(e)
+            return None
 
     @classmethod
     def read_png(cls, path: str) -> np.ndarray | None:
@@ -213,7 +224,7 @@ class ReadImage(Err):
         ext = ext.lower()
 
         data = {
-            ".psb": cls.read_psd,
+            ".psb": cls.read_psb,
             ".psd": cls.read_psd,
 
             ".tif": cls.read_tiff,
