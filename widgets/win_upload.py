02f7f272082364de93b2bbf645d7e6068438ed12
Evgeny Loshkarev <evlosh@gmail.com>
2025-03-07 11:10:20 +0300

    ref

--- widgets/win_upload.py
+++ widgets/win_upload.py
@@ -1,15 +1,13 @@
-from collections import defaultdict
 
 import sqlalchemy
 from PyQt5.QtCore import QObject, QTimer, pyqtSignal
-from PyQt5.QtWidgets import QListWidgetItem
 
 from base_widgets.wins import WinSystem
 from cfg import Dynamic
 from database import THUMBS, Dbase
-from utils.utils import URunnable, UThreadPool
+from utils.utils import URunnable, Utils
 
-from .menu_left import CollectionBtn, MenuLeft
+from .menu_left import MenuLeft, CollectionBtn
 
 
 class WorkerSignals(QObject):
@@ -37,7 +35,7 @@ class WinUpload(WinSystem):
 
         self.menu_left = MenuLeft()
         self.menu_left.tabBarClicked.disconnect()
-        self.menu_left.tabBarClicked.connect(self.tab_clicked)
+        # self.menu_left.tabBarClicked.connect(self.tab_clicked)
         self.central_layout.addWidget(self.menu_left)
         self.check_coll_btns()
 
@@ -62,7 +60,15 @@ class WinUpload(WinSystem):
 
             for i in coll_btns:
                 i.pressed_.disconnect()
-                i.pressed_.connect(lambda tt=i.text(): print(tt))
+                i.brand_ind = menu.brand_ind
+                cmd_ = lambda coll_btn=i: self.coll_btn_cmd(coll_btn=coll_btn)
+                i.pressed_.connect(cmd_)
 
     def tab_clicked(self, *args):
         print(args)
+
+    def coll_btn_cmd(self, coll_btn: CollectionBtn):
+        self.resize(self.menu_left.width() * 2, self.height())
+
+        coll_path = Utils.get_coll_folder(brand_ind=coll_btn.brand_ind)
+        print(coll_path)
