57add423fa9de8931fe402499c99cbd1e78fa004
Evgeny Loshkarev <evlosh@gmail.com>
2025-03-25 11:42:37 +0300

    ref

--- widgets/win_downloads.py
+++ widgets/win_downloads.py
@@ -146,10 +146,10 @@ class WinDownloads(WinSystem):
         self.v_layout.addWidget(self.progress_wid)
         self.v_layout.addStretch()
 
-        for i in range(0, 2):
-            wid_ = Progresser(text="test")
-            self.progress_layout.addWidget(wid_)
-            wid_.set_value.emit(50)
+        # for i in range(0, 2):
+        #     wid_ = Progresser(text="test")
+        #     self.progress_layout.addWidget(wid_)
+        #     wid_.set_value.emit(50)
 
         self.add_progress_widgets()
 
