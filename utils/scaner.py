662e0a6ae8d9b9690192b1fbd8c15db7316a9092
Evgeny Loshkarev <evlosh@gmail.com>
2025-06-19 15:20:21 +0300

    ref

--- utils/scaner.py
+++ utils/scaner.py
@@ -182,7 +182,7 @@ class DbImages:
 
 
 class Compator:
-    def __init__(self, finder_images: list, db_images: list):
+    def __init__(self, finder_images: dict, db_images: dictr):
         super().__init__()
         self.finder_images = finder_images
         self.db_images = db_images
