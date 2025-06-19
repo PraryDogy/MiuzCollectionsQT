1d7e5acb4c735dbc0b5bce4cd422f23b84317719
Morkik <evlosh@gmail.com>
2025-06-19 21:16:24 +0300

    ref

--- widgets/win_upload.py
+++ widgets/win_upload.py
@@ -1,106 +1,16 @@
 
 import os
 
-import sqlalchemy
-from PyQt5.QtCore import QObject, QSize, Qt, QTimer, pyqtSignal
+from PyQt5.QtCore import QSize, Qt, QTimer, pyqtSignal
 from PyQt5.QtWidgets import QLabel, QListWidget, QListWidgetItem, QWidget
 
 from base_widgets.layouts import LayoutHor
 from base_widgets.wins import WinSystem
 from cfg import Dynamic, Static
-from database import THUMBS, Dbase
 from lang import Lang
 from main_folders import MainFolder
-from utils.scaner import DbUpdater
-from utils.utils import Utils
 
-from ._runnable import URunnable, UThreadPool
 from .menu_left import CollectionBtn, MenuLeft
-
-
-class WorkerSignals(QObject):
-    finished_ = pyqtSignal()
-
-
-class DbUpdaterSetup(URunnable):
-    def __init__(self, urls: list[str], remove_urls: list[str] = None):
-        super().__init__()
-        self.urls = urls
-        self.remove_urls = remove_urls
-        self.signals_ = WorkerSignals()
-
-    def task(self):
-
-        MainFolder.current.check_avaiability()
-        if not MainFolder.current.get_current_path():
-            return
-
-        del_items = self.get_exist_records()
-        ins_items = self.get_new_records()
-
-        if self.remove_urls:
-            del_items.extend(self.get_remove_records())
-
-        db_updater = DbUpdater(del_items, ins_items, MainFolder.current)
-        db_updater.run()
-        self.signals_.finished_.emit()
-
-    def get_remove_records(self):
-        """
-        В приложении доступна функция "переместить" которая перемещает файлы
-        из одной директории в другую. Необходимо удалить из базы данных
-        записи об исходной директории файла, для этого подготавливаем список
-        для DbUpdater, который принимает список для удаления из short_hash
-        """
-        hashes: list[str] = []
-        if self.remove_urls:
-            for i in self.remove_urls:
-                hash_ = Utils.create_full_hash(i)
-                short_hash = Utils.get_short_hash(hash_)
-                hashes.append(short_hash)
-
-        return hashes
-
-    def get_new_records(self):
-        """
-        DbUpdater принимает список файлов для новых записей в базе данных
-        в следующем виде: (полный url файла, размер, создание, изменение)
-        """
-        ins_items: list[str] = []
-        for url in self.urls:
-            try:
-                stats = os.stat(url)    
-            except Exception as e:
-                Utils.print_error(e)
-                return
-            data = (url, stats.st_size, stats.st_birthtime, stats.st_mtime)
-            ins_items.append(data)
-        return ins_items
-
-    def get_exist_records(self) -> list[int]:
-        """
-        После того, как мы загрузили файлы на сетевой диск,
-        нам нужно обновить записи о файлах в базе данных.
-        Мы не используем функцию sqlalchemy.update, вместо этого используем
-        delete + insert, поэтому мы вычленяем из списка url те, которые уже 
-        есть в базе данных, чтобы удалить их
-        """
-        coll_folder = MainFolder.current.get_current_path()
-        short_urls = [
-            Utils.get_short_src(coll_folder, i)
-            for i in self.urls
-        ]
-        conn = Dbase.engine.connect()
-        q = sqlalchemy.select(THUMBS.c.short_hash).where(
-            sqlalchemy.and_(
-                THUMBS.c.short_src.in_(short_urls),
-                THUMBS.c.brand == MainFolder.current.name
-            )
-        )
-        try:
-            return conn.execute(q).scalars().all()
-        finally:
-            conn.close()
 
 
 class WinUpload(WinSystem):
