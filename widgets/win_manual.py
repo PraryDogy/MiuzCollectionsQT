5801a27b395c6d881a94faf5b4421ee94f478a74
PraryDogy <evlosh@gmail.com>
2025-06-29 19:17:10 +0300

    ref

--- widgets/win_manual.py
+++ widgets/win_manual.py
@@ -20,68 +20,35 @@ class WinFirstLoad(WinSystem):
         self.central_layout.setContentsMargins(10, 5, 10, 5)
         self.central_layout.setSpacing(5)
 
-        self.dynamic_page = self.page_zero()
-        self.central_layout.addWidget(self.dynamic_page)
-
         btn_wid_ = self.btn_wid()
-        self.central_layout.addWidget(btn_wid_)
-
-        self.central_layout.addStretch()
+        self.central_layout.insertWidget(1, btn_wid_)
 
         self.current_page = 0
         self.max_pages = 2
-        self.page_list = [self.page_zero, self.page_one, self.page_two]
-        
+        self.page_list = [
+            lambda: self.create_page(Lang.page_one, 0),
+            lambda: self.create_page(Lang.page_one, 1),
+            lambda: self.create_page(Lang.page_two, 2)
+        ]
 
+        self.dynamic_wid = self.page_list[0]()
+        self.central_layout.insertWidget(0, self.dynamic_wid)
         self.adjustSize()
 
-    def page_zero(self):
+    def create_page(self, text: str, page_num: int):
         v_wid = QWidget()
         v_lay = UVBoxLayout()
         v_lay.setSpacing(10)
         v_wid.setLayout(v_lay)
 
-        descr = QLabel(Lang.page_one_sec)
+        descr = QLabel(text)
         v_lay.addWidget(descr)
 
-        svg = self.get_svg_name(0)
+        svg = self.get_svg_name(page_num)
         svg = os.path.join(Static.images_dir, svg)
-        svg_wid = QSvgWidget()
-        svg_wid.load(svg)
-        svg_wid.setFixedSize(svg_wid.sizeHint())
-        v_lay.addWidget(svg_wid, alignment=Qt.AlignmentFlag.AlignCenter)
 
-        return v_wid
+        print(svg)
 
-    def page_one(self):
-        v_wid = QWidget()
-        v_lay = UVBoxLayout()
-        v_lay.setSpacing(10)
-        v_wid.setLayout(v_lay)
-
-        descr = QLabel(Lang.page_one_sec)
-        v_lay.addWidget(descr)
-
-        svg = self.get_svg_name(1)
-        svg = os.path.join(Static.images_dir, svg)
-        svg_wid = QSvgWidget()
-        svg_wid.load(svg)
-        svg_wid.setFixedSize(svg_wid.sizeHint())
-        v_lay.addWidget(svg_wid, alignment=Qt.AlignmentFlag.AlignCenter)
-
-        return v_wid
-    
-    def page_two(self):
-        v_wid = QWidget()
-        v_lay = UVBoxLayout()
-        v_lay.setSpacing(10)
-        v_wid.setLayout(v_lay)
-
-        descr = QLabel(Lang.page_two)
-        v_lay.addWidget(descr)
-
-        svg = self.get_svg_name(2)
-        svg = os.path.join(Static.images_dir, svg)
         svg_wid = QSvgWidget()
         svg_wid.load(svg)
         svg_wid.setFixedSize(svg_wid.sizeHint())
@@ -116,10 +83,10 @@ class WinFirstLoad(WinSystem):
             self.current_page -= 1
             self.next_btn.setDisabled(True)
         else:
-            self.dynamic_page.deleteLater()
+            self.dynamic_wid.deleteLater()
             new_wid = self.page_list[self.current_page]
-            self.dynamic_page = new_wid()
-            self.central_layout.insertWidget(1, self.dynamic_page)
+            self.dynamic_wid = new_wid()
+            self.central_layout.insertWidget(1, self.dynamic_wid)
 
     def get_svg_name(self, number: int):
         return f"example {number} {JsonData.lang_ind}.svg"
