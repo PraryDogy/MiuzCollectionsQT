3bd8bd428774c4250de33d17f9cdc76044ce9bf6
Evgeny Loshkarev <evlosh@gmail.com>
2025-07-22 10:12:04 +0300

    куа

--- test.py
+++ test.py
@@ -1,19 +1,10 @@
-from system.database import THUMBS, Dbase
-import sqlalchemy
-from system.utils import MainUtils
+from system.json_validator import JsonValidator
+from system.main_folder import MainFolder, MainFolderListModel
 
-Dbase.create_engine()
-conn = Dbase.engine.connect()
+src = MainFolder.json_file
+model = MainFolderListModel
+obj = MainFolder
 
-
-src = '/0 Other/1 IMG'
-
-q = sqlalchemy.select(THUMBS.c.short_src)
-q = q.where(THUMBS.c.short_src.ilike(f"{src}/%"))
-q = q.where(THUMBS.c.short_src.not_ilike(f"{src}/%/%"))
-
-res = conn.execute(q).scalars()
-
-for i in res:
-    if "коробки" in i:
-        print(i)
+validator = JsonValidator(src, obj, model)
+res = validator.get_validated_data()
+print(res)
