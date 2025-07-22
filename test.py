4343192e479272807ea859103ec5097ce875ca6c
Evgeny Loshkarev <evlosh@gmail.com>
2025-07-22 10:17:47 +0300

    ref

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 from system.json_validator import JsonValidator
-from system.main_folder import MainFolder, MainFolderListModel
+from system.main_folder import MainFolder, MainFolderListModel, MainFolderItemModel
 
 src = MainFolder.json_file
 model = MainFolderListModel
 obj = MainFolder
 
 validator = JsonValidator(src, obj, model)
-res = validator.get_validated_data()
-print(res)
+json_data = validator.load_json_data(src)
+for k, v in json_data.items():
+    for i in v:
+        validator = JsonValidator
