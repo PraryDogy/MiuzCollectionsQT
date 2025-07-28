1fd1f01f45201641ee149b880f39cb191507ec50
Evgeny Loshkarev <evlosh@gmail.com>
2025-07-28 12:31:43 +0300

    ref

--- system/scaner_task.py
+++ system/scaner_task.py
@@ -32,6 +32,7 @@ class ScanerTask(URunnable):
         self.signals_ = ScanerSignals()
         self.pause_flag = False
         self.user_canceled_scan = False
+        print("инициирован старый сканер")
 
     def task(self):
         main_folders = [
