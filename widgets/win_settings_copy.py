20764d70ea6a4f84c9ded855d9d98b63e6323467
Evgeny Loshkarev <evlosh@gmail.com>
2025-08-29 09:58:27 +0300

    ref

--- widgets/win_settings_copy.py
+++ widgets/win_settings_copy.py
@@ -1,7 +1,9 @@
+from copy import deepcopy
+
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QLabel, QPushButton, QSplitter, QTextEdit, QWidget
 
-from cfg import Static
+from cfg import JsonData, Static
 from system.lang import Lang
 from system.main_folder import MainFolder
 from system.utils import MainUtils
@@ -12,6 +14,9 @@ from ._base_widgets import UHBoxLayout, UListWidgetItem, VListWidget, WinChild
 class WinSettings(WinChild):
     def __init__(self, parent = None):
         super().__init__(parent)
+        self.main_folder_list = deepcopy(MainFolder.list_)
+        self.json_data = deepcopy(JsonData)
+
 
         self.splitter = QSplitter()
         self.splitter.setOrientation(Qt.Orientation.Horizontal)
