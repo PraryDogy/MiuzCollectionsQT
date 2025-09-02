ba1c610be4d8b9957c5a165e2705e235a9106e07
Evgeny Loshkarev <evlosh@gmail.com>
2025-09-02 11:48:01 +0300

    ref

--- widgets/win_upload.py
+++ widgets/win_upload.py
@@ -4,7 +4,7 @@ import subprocess
 
 from PyQt5.QtCore import Qt, pyqtSignal
 from PyQt5.QtGui import QMouseEvent
-from PyQt5.QtWidgets import QLabel, QPushButton, QTabWidget
+from PyQt5.QtWidgets import QGroupBox, QLabel, QPushButton, QTabWidget
 
 from cfg import JsonData, Static
 from system.lang import Lang
@@ -12,82 +12,18 @@ from system.main_folder import MainFolder
 from system.tasks import LoadCollListTask, LoadDirsTask
 from system.utils import UThreadPool
 
-from ._base_widgets import (UHBoxLayout, UListWidgetItem, UMenu, VListWidget,
-                            WinChild)
+from ._base_widgets import (UHBoxLayout, UListWidgetItem, UMenu, UVBoxLayout,
+                            VListWidget, WinChild)
 
 
-class SubWinList(VListWidget):
-    clicked = pyqtSignal(str)
-
-    def __init__(self, path: str):
-        super().__init__()
-        root = UListWidgetItem(self, text=os.path.basename(path))
-        root.path = path
-        self.addItem(root)
-        for i in os.scandir(path):
-            if i.is_dir():
-                item = UListWidgetItem(self, text=i.name)
-                item.path = i.path
-                self.addItem(item)
-        self.setCurrentRow(0)
-
-    def mouseReleaseEvent(self, e):
-        if e.button() == Qt.MouseButton.LeftButton:
-            path = self.currentItem().path
-            self.clicked.emit(path)
-        return super().mouseReleaseEvent(e)
-    
-
-class SubWin(WinChild):
-    clicked = pyqtSignal(str)
-
-    def __init__(self, path):
-        super().__init__()
-        self.setWindowFlags(Qt.WindowType.CustomizeWindowHint | Qt.WindowType.WindowCloseButtonHint)
-        self.v_list = SubWinList(path)
-        self.v_list.clicked.connect(self.clicked.emit)
-        self.central_layout.addWidget(self.v_list)
-
-    def keyPressEvent(self, a0):
-        if a0.key() == Qt.Key.Key_Escape:
-            self.deleteLater()
-        return super().keyPressEvent(a0)
-
-
-class CollBtn(QLabel):
-    pressed_ = pyqtSignal()
-    lang = (
-        ("Все коллекции", "All collections"),
-        ("Избранное", "Favorites"),
-    )
-
-    def __init__(self, text: str):
-        self.coll_name = text
-        data = {
-            Static.NAME_ALL_COLLS: self.lang[0][JsonData.lang],
-            Static.NAME_RECENTS: Lang.recents,
-            Static.NAME_FAVS: self.lang[1][JsonData.lang]
-        }
-        if text in data:
-            text = data.get(text)
-        if JsonData.abc_name:
-            text = re.sub(r'^[^A-Za-zА-Яа-я]+', '', text)
-        super().__init__(text=text)
-        self.setStyleSheet("padding-left: 5px;")
-
-    def mouseReleaseEvent(self, ev: QMouseEvent | None) -> None:
-        if ev.button() == Qt.MouseButton.LeftButton:
-            self.pressed_.emit()
-
-
-class CollBtnItem(UListWidgetItem):
+class DirsItem(UListWidgetItem):
     def __init__(self, parent: VListWidget, path: str, height = 30, text = None):
         super().__init__(parent, height, text)
         self.path = path
 
 
 class DirsList(VListWidget):
-    clicked = pyqtSignal(str)
+    set_path = pyqtSignal(str)
 
     def __init__(self, path: str):
         super().__init__()
@@ -102,15 +38,15 @@ class DirsList(VListWidget):
     
     def init_ui_fin(self, dirs: dict[str, str]):        
         self.clear()
-        back = CollBtnItem(parent=self, path=os.path.dirname(self.path), text="...")
+        back = DirsItem(parent=self, path=os.path.dirname(self.path), text="...")
         self.addItem(back)
         if self.path in self.main_folder_paths:
             for path, name in sorted(dirs.items(), key=lambda x: self._strip(x[1])):
-                item = CollBtnItem(parent=self, path=path, text=self._strip(name))
+                item = DirsItem(parent=self, path=path, text=self._strip(name))
                 self.addItem(item)
         else:
             for path, name in dirs.items():
-                item = CollBtnItem(parent=self, path=path, text=name)
+                item = DirsItem(parent=self, path=path, text=name)
                 self.addItem(item)
         self.setCurrentRow(0)
 
@@ -125,21 +61,23 @@ class DirsList(VListWidget):
     def _strip(self, s: str) -> str:
         return re.sub(r'^[^A-Za-zА-Яа-я]+', '', s)
 
-    def currentItem(self) -> CollBtnItem:
+    def currentItem(self) -> DirsItem:
         return super().currentItem()
-    
+        
     def mouseReleaseEvent(self, e):
-        e.ignore()
+        if e.button() != Qt.MouseButton.LeftButton:
+            return
+        item: DirsItem = self.itemAt(e.pos())
+        if item:
+            self.set_path.emit(item.path)
 
     def mouseDoubleClickEvent(self, e):
         if e.button() != Qt.MouseButton.LeftButton:
             return
-
         item = self.itemAt(e.pos())
         if item:
             self.path = item.path
             self.init_ui()
-
         return super().mouseDoubleClickEvent(e)
 
 
@@ -177,6 +115,27 @@ class MainFolderList(VListWidget):
 # ОСНОВНОЕ ОКНО ОСНОВНОЕ ОКНО ОСНОВНОЕ ОКНО ОСНОВНОЕ ОКНО ОСНОВНОЕ ОКНО ОСНОВНОЕ ОКНО 
 
 
+class InfoBox(QGroupBox):
+    lang = (
+        ("Копировать сюда:", "Copy here:"),
+    )
+    def __init__(self):
+        super().__init__()
+        v_lay = UVBoxLayout()
+        v_lay.setSpacing(5)
+        v_lay.setContentsMargins(5, 5, 5, 5)
+
+        self.label_top = QLabel(self.lang[0][JsonData.lang])
+        self.label_bottom = QLabel()
+
+        v_lay.addWidget(self.label_top)
+        v_lay.addWidget(self.label_bottom)
+        self.setLayout(v_lay)
+
+    def set_path(self, path: str):
+        self.label_bottom.setText(path)
+
+
 class WinUpload(WinChild):
     clicked = pyqtSignal(tuple)
     lang = (
@@ -189,10 +148,11 @@ class WinUpload(WinChild):
         super().__init__()
         self.resize(350, 500)
         self.setWindowFlags(Qt.WindowType.CustomizeWindowHint | Qt.WindowType.WindowCloseButtonHint)
-        self.tab_wid = QTabWidget()
-        self.central_layout.addWidget(self.tab_wid)
         self.central_layout.setSpacing(5)
         self.central_layout.setContentsMargins(5, 5, 5, 5)
+
+        self.tab_wid = QTabWidget()
+        self.central_layout.addWidget(self.tab_wid)
 
         self.main_folders = MainFolderList()
         self.main_folders.clicked.connect(self.main_folder_click)
@@ -200,6 +160,12 @@ class WinUpload(WinChild):
 
         self.dirs_list = DirsList(MainFolder.current.curr_path)
         self.tab_wid.addTab(self.dirs_list, self.lang[0][JsonData.lang])
+
+        # новый бокс над кнопками
+        self.info_box = InfoBox()
+        self.info_box.set_path(MainFolder.current.curr_path)
+        self.central_layout.addWidget(self.info_box)
+        self.dirs_list.set_path.connect(self.info_box.set_path)
 
         # кнопки внизу
         btn_lay = UHBoxLayout()
