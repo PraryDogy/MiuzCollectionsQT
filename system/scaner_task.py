9fd3a20929363811fa3131f6c2ce886bb57858ab
Pupitor 9000 <evlosh@gmail.com>
2025-09-13 18:47:27 +0300

    ref

--- system/scaner_task.py
+++
@@ -1,8 +0,0 @@
-from cfg import Cfg
-
-if Cfg.new_scaner:
-    from .new_scaner.scaner_task import ScanerTask
-    class ScanerTask(ScanerTask): ...
-else:
-    from .old_scaner.scaner_task import ScanerTask
-    class ScanerTask(ScanerTask): ...
