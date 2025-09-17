46b0a452209bbc1a5642d08dd5de50a6a02e81f7
Evgeny Loshkarev <evlosh@gmail.com>
2025-09-17 17:55:24 +0300

    ref

--- image_counter.py
+++ image_counter.py
@@ -4,9 +4,9 @@ import sys
 import cv2
 import numpy as np
 from PyQt5.QtCore import QObject, QRunnable, Qt, QThreadPool, pyqtSignal
-from PyQt5.QtWidgets import (QApplication, QDialog, QGridLayout, QHBoxLayout,
-                             QLabel, QPushButton, QTextEdit, QVBoxLayout,
-                             QWidget)
+from PyQt5.QtWidgets import (QAction, QApplication, QDialog, QGridLayout,
+                             QHBoxLayout, QLabel, QMenu, QPushButton,
+                             QTextEdit, QVBoxLayout, QWidget)
 
 
 class ColorHighlighter(QRunnable):
@@ -14,13 +14,10 @@ class ColorHighlighter(QRunnable):
     class Sigs(QObject):
         finished_ = pyqtSignal(list)
 
-    search_colors = {
-        "Синий": (np.array([100, 80, 80]), np.array([140, 255, 255])),
-    }
-
-    def __init__(self, files: list[str]):
+    def __init__(self, files: list[str], selected_colors: dict):
         super().__init__()
         self.files = files
+        self.selected_colors = selected_colors
         self.sigs = ColorHighlighter.Sigs()
 
     def run(self):
@@ -39,18 +36,17 @@ class ColorHighlighter(QRunnable):
         image = cv2.imread(file)
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         output = image.copy()
+        filled_mask = np.zeros((image.shape[0], image.shape[1]), dtype=np.uint8)
 
-        for color_name, (lower, upper) in self.search_colors.items():
+        for color_name, (lower, upper) in self.selected_colors.items():
             mask = cv2.inRange(hsv, lower, upper)
             contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-            filled_mask = np.zeros_like(mask)
             for cnt in contours:
                 if cv2.contourArea(cnt) >= min_area:
-                    cv2.drawContours(output, [cnt], -1, (0, 0, 255), cv2.FILLED)  # красная заливка
+                    cv2.drawContours(output, [cnt], -1, (0, 0, 255), cv2.FILLED)
                     cv2.drawContours(filled_mask, [cnt], -1, 255, cv2.FILLED)
 
-            percent = (cv2.countNonZero(filled_mask) / (image.shape[0] * image.shape[1])) * 100
+        percent = (cv2.countNonZero(filled_mask) / (image.shape[0] * image.shape[1])) * 100
 
         return round(percent, 2), output
 
@@ -84,14 +80,6 @@ class ResultsDialog(QDialog):
         self.percent_edit.setFrameStyle(0)
         self.percent_edit.setText("\n".join(str(f[1]) for f in files))
         grid.addWidget(self.percent_edit, 1, 1)
-
-        # Третий "столбец" — кнопки
-        btns_layout = QVBoxLayout()
-        for filename, _, img in files:
-            btn = QPushButton("Просмотр")
-            btn.clicked.connect(lambda _, img=img, name=filename: self.show_image(img, name))
-            btns_layout.addWidget(btn)
-        grid.addLayout(btns_layout, 1, 2)
 
     def show_image(self, img, name: str):
         if img is None:
@@ -129,26 +117,69 @@ class FileDropTextEdit(QTextEdit):
             super().dropEvent(event)
 
 
+class ColorAction(QAction):
+    def __init__(self, parent, text):
+        super().__init__(parent=parent, text=text)
+        self.value: tuple[np.array, np.array]
+        self.color_name: str
+
+
 class MainWindow(QWidget):
+    search_colors = {
+        "Синий": (np.array([100, 80, 80]), np.array([140, 255, 255])),
+        "Жёлтый": (np.array([20, 100, 100]), np.array([30, 255, 255])),
+    }
+
     def __init__(self):
         super().__init__()
         self.setWindowTitle("File Drop Example")
         self.resize(400, 300)
+        self.selected_colors = {}
 
         layout = QVBoxLayout(self)
+
+        self.color_btn = QPushButton("цвета")
+        self.color_btn.setFixedWidth(100)
+        self.color_btn.clicked.connect(self.show_menu)
+        layout.addWidget(self.color_btn, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        self.color_menu = QMenu(parent=self.color_btn)
+        self.color_menu.setMinimumWidth(150)
+        for color_name, value in self.search_colors.items():
+            act = ColorAction(self.color_menu, color_name)
+            act.setCheckable(True)
+            act.value = value
+            act.color_name = color_name
+            act.triggered.connect(lambda e, act=act: self.color_action(act))
+            if act.color_name in self.selected_colors:
+                act.setChecked(True)
+            self.color_menu.addAction(act)
 
         self.text_edit = FileDropTextEdit(self)
         layout.addWidget(self.text_edit)
 
         self.start_btn = QPushButton("Старт", self)
+        self.start_btn.setFixedWidth(100)
         self.start_btn.clicked.connect(self.cmd)
-        layout.addWidget(self.start_btn)
+        layout.addWidget(self.start_btn, alignment=Qt.AlignmentFlag.AlignCenter)
 
+    def color_action(self, action: ColorAction):
+        if action.color_name in self.selected_colors:
+            self.selected_colors.pop(action.color_name)
+            action.setChecked(False)
+        else:
+            self.selected_colors[action.color_name] = action.value
+            action.setChecked(True)
+
+    def show_menu(self):
+        self.color_menu.exec_(self.color_btn.mapToGlobal(self.color_btn.rect().bottomLeft()))
     
     def cmd(self):
-        self.start_btn.setDisabled(True)
         files = self.text_edit.toPlainText().split("\n")
-        task = ColorHighlighter(files)
+        if not files or not self.selected_colors:
+            return
+        self.start_btn.setDisabled(True)
+        task = ColorHighlighter(files, self.selected_colors)
         task.sigs.finished_.connect(self.finished)
         task.run()
 
@@ -156,8 +187,6 @@ class MainWindow(QWidget):
         self.start_btn.setDisabled(False)
         self.result = ResultsDialog(files)
         self.result.show()
-        # for filename, percent, array_img in files:
-        #     print(filename, percent)
 
 
 if __name__ == "__main__":
