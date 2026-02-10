8163717a1a9316ed63ec27d6704b4e075bf12450
PraryDogy <evlosh@gmail.com>
2026-02-10 18:25:41 +0300

    ref

--- system/items.py
+++ system/items.py
@@ -1,5 +1,5 @@
 import os
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from multiprocessing import Queue
 from typing import Literal
 
@@ -7,22 +7,6 @@ import numpy as np
 import sqlalchemy
 
 from .main_folder import Mf
-
-# class CopyTaskItem:
-#     def __init__(self, src_dir: str, dst_dir: str, src_urls: list[str], is_search: bool, is_cut: bool):
-#         super().__init__()
-#         self.src_dir = src_dir
-#         self.dst_dir = dst_dir
-#         self.src_urls = src_urls
-#         self.is_search = is_search
-#         self.is_cut = is_cut
-
-#         self.current_size: int = 0
-#         self.total_size: int = 0
-#         self.current_count: int = 0
-#         self.total_count: int = 0
-#         self.dst_urls: list[str] = []
-#         self.msg: Literal["", "error", "need_replace", "replace_one", "replace_all", "finished"]
 
 
 @dataclass(slots=True)
@@ -45,20 +29,6 @@ class NeedResetItem:
 
 
 @dataclass(slots=True)
-class CopyTaskItem:
-    dst_dir: str
-    src_urls: list[str]
-    is_cut: bool
-
-    current_size: int = 0
-    total_size: int = 0
-    current_count: int = 0
-    total_count: int = 0
-    dst_urls: list[str] = field(default_factory=list)
-    msg: Literal["", "error", "need_replace", "replace_one", "replace_all", "finished"] = ""
-
-
-@dataclass(slots=True)
 class OnStartItem:
     mf_list: list["Mf"]
 
@@ -77,3 +47,18 @@ class ScanerItem:
         else:
             self.mf_real_name = os.path.basename(mf.paths[0])
         self.total_count = 0
+
+
+class CopyTaskItem:
+    def __init__(self, dst_dir: str, src_urls: list[str], is_cut: bool):
+        super().__init__()
+        self.dst_dir = dst_dir
+        self.src_urls = src_urls
+        self.is_cut = is_cut
+
+        self.current_size: int = 0
+        self.total_size: int = 0
+        self.current_count: int = 0
+        self.total_count: int = 0
+        self.dst_urls: list[str] = []
+        self.msg: Literal["", "error", "need_replace", "replace_one", "replace_all", "finished"] = ""
