f3c43fe61f18165269230c2825c5719e72f634e6
PraryDogy <evlosh@gmail.com>
2026-03-11 16:02:35 +0300

    куа

--- test 2.py
+++ test 2.py
@@ -1,4 +1,4 @@
-def test(number_list: list[str]):
+def test(number_list):
     counter = 0
     for i in range(1, len(number_list)):
         prev = number_list[i - 1]
@@ -8,6 +8,6 @@ def test(number_list: list[str]):
     return counter
 
 
-number_list = input().split()
+number_list = [int(i) for i in input().split()]
 counter = test(number_list)
 print(counter)
