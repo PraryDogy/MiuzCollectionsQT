c9ce5a494e74e10e4b4eb3d8c2b91bcada1ae69e
Evgeny Loshkarev <evlosh@gmail.com>
2026-03-12 09:35:54 +0300

    ref

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 from system.shared_utils import ImgUtils
-
+import cv2
 
 src = "/Users/Loshkarev/Desktop/2022_MIUZ_3005_7195.psb"
 img = ImgUtils.read_img(
     src
 )
+
+cv2.imshow("1", img)
+cv2.waitKey(0)
