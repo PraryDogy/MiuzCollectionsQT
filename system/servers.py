aa4db967ba647d49932545c3a2c2f9e650e3978e
Evgeny Loshkarev <evlosh@gmail.com>
2026-03-13 12:39:36 +0300

    ref

---
+++ system/servers.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+from cfg import Static
+
+
+class Servers:
+    server_list = []
+
+    @classmethod
+    def init(cls):
+        if os.path.exists(Static.external_servers):
+            try:
+                with open(Static.external_servers, "r", encoding="utf-8") as f:
+                    server_list: list = json.load(f)
+                for i in server_list:
+                    Servers.server_list.append(i)
+            except Exception as e:
+                print("servers error", e)
+    
+    @classmethod
+    def write_json_data(cls):
+        with open(Static.external_servers, "w", encoding="utf-8") as file:
+            json.dump(cls.server_list, file, indent=4, ensure_ascii=False)
