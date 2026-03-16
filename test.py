7ca1e54963189a9b544b401d41fed470a41aba57
Evgeny Loshkarev <evlosh@gmail.com>
2026-03-16 12:06:59 +0300

    ref

--- test.py
+++
@@ -1,9 +0,0 @@
-src = "/Users/Loshkarev/Documents/_Projects/MiuzCollectionsQT/images/broken_image.jpg"
-
-
-from system.shared_utils import ImgUtils
-import cv2
-
-img = ImgUtils._get_broken_image()
-cv2.imshow("123", img)
-cv2.waitKey(0)
