e853dd9b770de79a8815f5a4ea33c4e8b1b7d8e3
PraryDogy <evlosh@gmail.com>
2026-03-25 22:26:47 +0300

    ref

--- widgets/win_main.py
+++ widgets/win_main.py
@@ -224,7 +224,7 @@ class WinMain(UMainWindow):
         self.grid.setFocus()
 
         self.grid.reload_thumbnails()
-        if argv[-1] != "noscan":
+        if "noscan" in argv:
             self.start_scaner_task()
         self.start_wachdog()
 
