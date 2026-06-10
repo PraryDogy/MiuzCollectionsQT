c137603078004054b8fa40677f97b8cdedec0c78
Evgeny Loshkarev <evlosh@gmail.com>
2026-06-10 12:09:35 +0300

    ref

--- widgets/win_settings.py
+++ widgets/win_settings.py
@@ -266,6 +266,8 @@ class RebootSettings(GroupWid):
             )
             with zipfile.ZipFile(zip_path, "r") as z:
                 z.extractall(Static.external_files_dir)
+            if Static.app_ver > Cfg.app_ver:
+                Cfg.app_ver = Static.app_ver
             os.remove(zip_path)
             restart_app()
 
