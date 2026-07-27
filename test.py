5004f0fa4bcc38d0816e8e899287d8a060fa0bf9
PraryDogy <evlosh@gmail.com>
2026-07-27 15:28:43 +0300

    ref

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
-a = './icons/bar_top/sort.svg'
+class JsonData:
+    __slots__ = ("app_ver", "lng_index", "theme", "scaner_minutes", "hide_digits_mf_lst")
+    app_ver = 123
+    lng_index = 0
+    theme = 11111
+    scaner_minutes = 20
+    hide_digits_mf_lst = []
+
+    @classmethod
+    def get_data(cls):
+        return {
+            i: getattr(cls, i)
+            for i in cls.__slots__
+        }
 
 
-import os
-print(os.path.exists(a))
+print(JsonData.get_data())
