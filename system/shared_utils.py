735052d83f4b565a52f8dd8caa8c4801bf81e072
Evgeny Loshkarev <evlosh@gmail.com>
2026-09-07 09:47:18 +0300

    is grayscale > simplier

--- system/shared_utils.py
+++ system/shared_utils.py
@@ -556,6 +556,7 @@ class ImgUtils:
     def is_grayscale(img: np.ndarray, saturation_threshold=3):
         if len(img.shape) < 3 or img.shape[2] == 1:
             return True
+        return False
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         _, s, _ = cv2.split(hsv)
         mean_saturation = np.mean(s)
