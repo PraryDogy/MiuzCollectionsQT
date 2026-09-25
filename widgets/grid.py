32ab043a5b07ea3336d300f85e52456e796df23e
Evgeny Loshkarev <evlosh@gmail.com>
2026-09-25 11:18:09 +0300

    ref

--- widgets/grid.py
+++ widgets/grid.py
@@ -242,6 +242,11 @@ class UpBtn(QSvgWidget):
         super().mouseReleaseEvent(ev)
 
 
+class GridWid(UFrame):
+    def __init__(self):
+        super().__init__()
+
+
 class Grid(VScrollArea):
     load_st_grid = pyqtSignal()
     restart_scaner = pyqtSignal()
@@ -294,7 +299,7 @@ class Grid(VScrollArea):
         self.date_timer.setSingleShot(True)
 
         # --- Вкладка прокрутки ---
-        self.scroll_wid = UFrame()
+        self.scroll_wid = GridWid()
         self.setWidget(self.scroll_wid)
         self.scroll_layout = QVBoxLayout(self.scroll_wid)
         self.scroll_layout.setContentsMargins(0, 0, 0, 0)
