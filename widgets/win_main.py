e4ff4ee5021d7f6fbeeb2207253772e638cccc28
Evgeny Loshkarev <evlosh@gmail.com>
2026-09-25 13:30:45 +0300

    ref

--- widgets/win_main.py
+++ widgets/win_main.py
@@ -320,24 +320,12 @@ class WinMain(UMainWindow):
         
     def open_filters_win(self):
 
-        def on_closed():
-            if not any((
-                Dynamic.filter_only_folder,
-                Dynamic.filter_favs,
-                *Dynamic.word_tags,
-                Dynamic.date_start,
-                Dynamic.date_end
-            )):
-                self.bar_top.filters_btn.set_base_style()
-
         def edit_filters():
             settings_item = SettingsItem("filters", "")
-            # self.filters_win.deleteLater()
             self.open_settings_win(settings_item)
 
         self.bar_top.filters_btn.set_selected_style()
         self.filters_win = WinFilters()
-        self.filters_win.closed_.connect(on_closed)
         self.filters_win.reload_thumbnails.connect(self.load_st_grid)
         self.filters_win.edit_filters.connect(edit_filters)
         self.filters_win.center_to_parent(self.window())
